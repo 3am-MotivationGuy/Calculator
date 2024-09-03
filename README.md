6f121a92455b187db458885b306e62d6ab2858de
3am-MotivationGuy <kushagrasharmaa@gmail.com>
2024-09-03 21:47:17 +0530

    Completed main file with changes to README file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is my second real project.
 I dont know if i can call this a "project". I made this as a way to clear up some syntax and
 stuff about java. 
+Feedback and advice is absolutely appreciated since I'm just a beginner.
+Thank you!
--- main.java
+++ main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 class Main{
     public static void Main(){
+        Volume v = new Volume();
+        Area a = new Area();
         Scanner sc = new Scanner(System.in)
         System.out.println("Welcome to the calculator alpha version!");
         System.out.println("What would you like to do?\n1 - Area of a shape\n2 - Volume of a shape");
@@ -12,7 +14,29 @@ class Main{
             case 1: System.out.println("Which shape?");
                     String shape = sc.next();
                     switch(shape){
-                        case "square": 
+                        case "square": System.out.println("Area of Square is" + (a.Square()));
+                                        break;
+                        case "rectangle": System.out.println("Area of rectangle is" + (a.Rectangle()));
+                                        break;
+                        case "triangle": System.out.println("Area of triangle is "+ (a.Triangle()));
+                                        break;
+                        case "circle": System.out.println("Area of circle is" + (a.Circle()));
+                                        break;
+                    }
+                    break;
+            case 2: System.out.println("Which shape?");
+                    String shape = sc.next();
+                    switch(shape){
+                        case "cube": System.out.println("Volume of cube is" + (v.Cube()));
+                                    break;
+                        case "cuboid": System.out.println("Volume of cuboid is " + (v.Cuboid()));
+                                    break;
+                        case "cone": System.out.println("Volume of Cone is " + (v.Cone()));
+                                    break;
+                        case "cylinder": System.out.println("Volume of Cylinder is" + (v.Cylinder()));
+                                    break;
+                        case "sphere": System.out.println("Volume of sphere is " + (v.Sphere()));
+                                    break;
                     }
         }
     }
